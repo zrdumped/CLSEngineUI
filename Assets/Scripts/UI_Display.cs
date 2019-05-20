1f42a981b7f85c3a5cb73b1a53c226691d46623a
Mr.Melon <nmgzhangran@163.com>
2019-05-20 12:55:27 +0800

    Add cave & VR

---
+++ Assets/Scripts/UI_Display.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Display : MonoBehaviour {
+
+    public GameObject caveCameras;
+    public GameObject caveLookCamera;
+    public GameObject mainCamera;
+    public GameObject VRCameras;
+    public GameObject VRLookCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Back()
+    {
+        caveLookCamera.SetActive(false);
+        VRLookCamera.SetActive(false);
+        mainCamera.SetActive(true);
+    }
+
+    public void toCave()
+    {
+        VRLookCamera.SetActive(false);
+        mainCamera.SetActive(false);
+        caveLookCamera.SetActive(true);
+    }
+
+    public void toVR()
+    {
+        caveLookCamera.SetActive(false);
+        mainCamera.SetActive(false);
+        VRLookCamera.SetActive(true);
+    }
+}
